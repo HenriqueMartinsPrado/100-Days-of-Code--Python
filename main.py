9f99ce847ee9e85842d7f657e08582efaeb857f0
HenriqueMartinsPrado <45240559+HenriqueMartinsPrado@users.noreply.github.com>
2023-07-19 01:30:06 +0000

    Day 2

--- main.py
+++ main.py
@@ -1,1 +1,52 @@
+# Day 1
+
+# The name of the program to debug is Thonny
+
 # Write your code below this line 👇
+print("Hello World!")
+print("Hello World!")
+print("Hello World!")
+
+print("Hello World!\nHello World!\nHello World!")
+
+print("Henrique"+" Martins")
+
+input("What is your name?")
+len(input("What is your name?"))
+print(len(input("What is your name?")))
+
+# Project
+
+print("\n=========== Project Day 1 ===========")
+city_name = input("What city did you grow up?\n")
+pet_name = input("What is the name of your pet?\n")
+
+band = city_name + pet_name
+
+print(band)
+
+# ==================================================================
+#  Day 2
+
+# Data Types
+
+# String
+
+print("Hello"[0])
+print("123" + "456")
+print("hello" + "world")
+
+# Integer
+
+print(123 + 456)
+
+# To better read the number like 123,456,789, you can user _. The exemplo is goino to be 123_456_789. The python language will interpret that like 123456789
+
+# Float
+
+123.45 
+
+# Boolean
+
+True 
+False
