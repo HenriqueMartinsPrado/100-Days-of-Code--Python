4755e8942f167fd417e8c2d8ae0e96341448a6e9
HenriqueMartinsPrado <45240559+HenriqueMartinsPrado@users.noreply.github.com>
2023-07-22 01:44:49 +0000

    day 5 v3

--- main.py
+++ main.py
@@ -411,3 +411,28 @@ for student in student_heights:
 
 avg = int(count/number_of_students)
 print(avg)
+
+
+
+
+
+
+# ex3
+
+# 🚨 Don't change the code below 👇
+student_scores = input("Input a list of student scores ").split()
+for n in range(0, len(student_scores)):
+  student_scores[n] = int(student_scores[n])
+print(student_scores)
+# 🚨 Don't change the code above 👆
+
+#Write your code below this row 👇
+
+high = 0
+for score in student_scores:
+    if high < score:
+        high = score
+
+print(f"The highest score in the class is: {high}")
+
+
