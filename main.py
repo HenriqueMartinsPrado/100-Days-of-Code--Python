b8b7d52572434733a228cc83e80f0d00cf1d6745
HenriqueMartinsPrado <45240559+HenriqueMartinsPrado@users.noreply.github.com>
2023-07-22 04:18:09 +0000

    day 5 v8

--- main.py
+++ main.py
@@ -476,25 +476,42 @@ numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the PyPassword Generator!")
-nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_letters = int(input("How many letters would you like in your password?\n")) 
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-#Eazy Level - Order not randomised:
-#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
-psswd = ""
+#Eazy Level
+# password = ""
+
+# for char in range(1, nr_letters + 1):
+#   password += random.choice(letters)
+
+# for char in range(1, nr_symbols + 1):
+#   password += random.choice(symbols)
+
+# for char in range(1, nr_numbers + 1):
+#   password += random.choice(numbers)
+
+# print(password)
+
+#Hard Level
+password_list = []
+
 for char in range(1, nr_letters + 1):
-    psswd += random.choice(letters)
-    print(psswd)
+  password_list.append(random.choice(letters))
 
 for char in range(1, nr_symbols + 1):
-    psswd += random.choice(symbols)
-    print(psswd)
+  password_list += random.choice(symbols)
 
 for char in range(1, nr_numbers + 1):
-    psswd += random.choice(numbers)
-    print(psswd)
+  password_list += random.choice(numbers)
 
+print(password_list)
+random.shuffle(password_list)
+print(password_list)
 
-#Hard Level - Order of characters randomised:
-#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+password = ""
+for char in password_list:
+  password += char
+
+print(f"Your password is: {password}")
