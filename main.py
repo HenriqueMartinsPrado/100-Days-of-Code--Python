61c6c92f03ef1b3e904eae6facf6fa0d44c79c73
HenriqueMartinsPrado <45240559+HenriqueMartinsPrado@users.noreply.github.com>
2023-07-22 03:50:56 +0000

    day 5 v6

--- main.py
+++ main.py
@@ -452,11 +452,11 @@ print(even_sum)
 
 n = 0
 for n in range(1, 101):
-    if n % 3 == 0:
-        print("Fizz")
-    elif n % 5 == 0: 
-        print("Buzz")
-    elif n % 3 == 0 and n % 5 == 0:
+    if n % 3 == 0 and n % 5 == 0:
         print("FizzBuzz")
+    elif n % 3 == 0: 
+        print("Fizz")
+    elif n % 5 == 0:
+        print("Buzz")
     else:
         print(f"{n}")
