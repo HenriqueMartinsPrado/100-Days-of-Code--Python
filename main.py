e7b091f1e442317828cb65cbb39e3efb37c40df5
HenriqueMartinsPrado <45240559+HenriqueMartinsPrado@users.noreply.github.com>
2023-07-25 21:02:07 +0000

    day 8 v2

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 # ==================================================================
 # Day 8
 
+# name = input("What is your name? ")
+
+# def my_function(name):
+#  print(f"Hello {name}")
+
+# my_function(name)
+
+# ========
+
 name = input("What is your name? ")
+location = input("What are you right now? ")
 
-def my_function(name):
-  print(f"Hello {name}")
+def greet(name, location):
+  print(f"Hi {name}")
+  print(f"We added your location {location} in our databases")
 
-my_function(name)
-
+greet(location=location, name=name)
