5c7ba167d82d18ba1dd9687b65b526f73c02cc13
HenriqueMartinsPrado <45240559+HenriqueMartinsPrado@users.noreply.github.com>
2023-07-25 21:33:50 +0000

    day 8 v3

--- main.py
+++ main.py
@@ -1,20 +1,641 @@
+# Day 1
+
+# The name of the program to debug is Thonny
+
+# Write your code below this line 👇
+print("Hello World!")
+print("Hello World!")
+print("Hello World!")
+
+print("Hello World!\nHello World!\nHello World!")
+
+print("Henrique"+" Martins")
+
+input("What is your name?")
+len(input("What is your name?"))
+print(len(input("What is your name?")))
+
+# Project
+
+print("\n=========== Project Day 1 ===========")
+city_name = input("What city did you grow up?\n")
+pet_name = input("What is the name of your pet?\n")
+
+band = city_name + pet_name
+
+print(band)
+
+# ==================================================================
+#  Day 2 
+
+# Data Types
+
+# String
+
+print("Hello"[0])
+print("123" + "456")
+print("hello" + "world")
+
+# Integer
+
+print(123 + 456)
+
+# To better read the number like 123,456,789, you can user _. The exemplo is goino to be 123_456_789. The python language will interpret that like 123456789
+
+# Float
+
+123.45 
+
+# Boolean 
+
+True 
+False
+
+# ==================================================================
+# Day 3 
+
+# Conditions
+
+# ex1
+# 🚨 Don't change the code below 👇
+number = int(input("Which number do you want to check? "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+if (number % 2) == 0:
+    print("This is an even number")
+else:
+    print("This is an odd number.")
+
+
+
+
+
+# ex2
+# 🚨 Don't change the code below 👇
+height = float(input("enter your height in m: "))
+weight = float(input("enter your weight in kg: "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+import math
+
+bmi = math.ceil(weight / (height * height))
+#print(f"Your BMI is {bmi}")
+
+if bmi < 18.5:
+    print(f"Your BMI is {bmi}, you are underweight.")
+elif bmi >= 18.5 and bmi < 25:
+    print(f"Your BMI is {bmi}, you have a normal weight.")
+elif bmi >= 25 and bmi < 30:
+    print(f"Your BMI is {bmi}, you are slightly overweight.")
+elif bmi >= 30 and bmi < 35:
+    print(f"Your BMI is {bmi}, you are obese.")
+else:
+    print(f"Your BMI is {bmi}, you are clinically obese.")
+
+
+
+
+
+# ex3 
+# 🚨 Don't change the code below 👇
+year = int(input("Which year do you want to check? "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+if (year % 4) == 0:
+    if (year % 100) == 0:
+        if (year % 400) == 0:
+            print("Leap year.")
+        else:
+            print("Not leap year.")
+    else:
+        print("Leap year.")
+else:
+    print("Not leap year.")
+
+
+
+
+
+# ex4
+# 🚨 Don't change the code below 👇
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L ")
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+bill = 0
+
+if size == "S":
+    bill = 15
+    if add_pepperoni ==  "Y":
+        bill += 2
+        if extra_cheese == "Y":
+            bill += 1     
+            print(f"Your final bill is: ${bill}.")
+        else:
+            print(f"Your final bill is: ${bill}.")
+    else:
+        if extra_cheese == "Y":
+            bill += 1     
+            print(f"Your final bill is: ${bill}.")
+        else:
+            print(f"Your final bill is: ${bill}.")
+elif size == "M":
+    bill = 20
+    if add_pepperoni ==  "Y":
+        bill += 3
+        if extra_cheese == "Y":
+            bill += 1     
+            print(f"Your final bill is: ${bill}.")
+        else:
+            print(f"Your final bill is: ${bill}.")
+    else:
+        if extra_cheese == "Y":
+            bill += 1     
+            print(f"Your final bill is: ${bill}.")
+        else:
+            print(f"Your final bill is: ${bill}.")
+else:
+    bill = 25
+    if add_pepperoni ==  "Y":
+        bill += 3
+        if extra_cheese == "Y":
+            bill += 1     
+            print(f"Your final bill is: ${bill}.")
+        else:
+            print(f"Your final bill is: ${bill}.")
+    else:
+        if extra_cheese == "Y":
+            bill += 1     
+            print(f"Your final bill is: ${bill}.")
+        else:
+            print(f"Your final bill is: ${bill}.")
+
+
+
+
+
+# ex 5
+# 🚨 Don't change the code below 👇
+print("Welcome to the Love Calculator!")
+name1 = input("What is your name? \n")
+name2 = input("What is their name? \n")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+# convert to lower strings
+name1 = name1.lower()
+name2 = name2.lower()
+
+# combining both
+
+comb = name1+name2
+
+# count TRUE and LOVE
+t = comb.count("t")
+r = comb.count("r")
+u = comb.count("u")
+e = comb.count("e")
+total_true = t+r+u+e
+
+l = comb.count("l")
+o = comb.count("o")
+v = comb.count("v")
+e = comb.count("e")
+total_love = l+o+v+e
+
+result = int(str(total_true) + str(total_love))
+
+if result < 10 or result > 90:
+    print(f"Your score is {result}, you go together like coke and mentos.")
+elif result >= 40 and result <=50:
+    print(f"Your score is {result}, you are alright together.")
+else:
+    print(f"Your score is {result}.")
+
+
+
+
+# ex 6
+
+choice1 = input('You\'re at a cross road. Where do you want to go? Type "left" or "right" \n').lower()
+if choice1 == "left":
+  choice2 = input('You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across. \n').lower()
+  if choice2 == "wait":
+    choice3 = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose? \n").lower()
+    if choice3 == "red":
+      print("It's a room full of fire. Game Over.")
+    elif choice3 == "yellow":
+      print("You found the treasure! You Win!")
+    elif choice3 == "blue":
+      print("You enter a room of beasts. Game Over.")
+    else:
+      print("You chose a door that doesn't exist. Game Over.")
+  else:
+    print("You get attacked by an angry trout. Game Over.")
+else:
+  print("You fell into a hole. Game Over.")
+
+
+# ==================================================================
+# Day 4
+
+# Tips
+import random
+import module_teste
+
+print(module_teste.name)
+
+# ex1
+import random
+
+coin = random.randint(0,1)
+
+if coin == 0:
+    print("Heads")
+else:
+    print("Tails")
+
+
+
+# ex2
+# Import the random module here
+
+# Import the random module here
+
+import random
+
+# Split string method
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+num_itens = len(names)
+
+# Generate a random numbers between 0 and the last index
+random_choice = random.randint(0, num_itens - 1)
+person = names[random_choice]
+print(f"{person} is going to buy the meal today!")
+
+
+# Using choice
+# random_name = random.choice(names)
+# print(f"{random_name} is going to buy the meal today!")
+
+
+import random
+
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+ 
+dirty_dozen = [fruits, vegetables]
+ 
+print(dirty_dozen[1])
+print(dirty_dozen[1][1])
+
+
+# ex3
+# Go to https://replit.com/@appbrewery/rock-paper-scissors-start?v=1
+
+import random
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+game_images = [rock, paper, scissors]
+
+user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+print(game_images[user_choice])
+
+computer_choice = random.randint(0, 2)
+print("Computer chose:")
+print(game_images[computer_choice])
+
+if user_choice >= 3 or user_choice < 0: 
+  print("You typed an invalid number, you lose!") 
+elif user_choice == 0 and computer_choice == 2:
+  print("You win!")
+elif computer_choice == 0 and user_choice == 2:
+  print("You lose")
+elif computer_choice > user_choice:
+  print("You lose")
+elif user_choice > computer_choice:
+  print("You win!")
+elif computer_choice == user_choice:
+  print("It's a draw")
+
+####### Debugging challenge: #########
+#Try running this code and type 5.
+#It will give you an IndexError and point to line 32 as the issue.
+#But on line 38 we are trying to prevent a crash by detecting
+#any numbers great than or equal to 3 or less than 0.
+#So what's going on?
+#Can you debug the code and fix it?
+#Solution: https://repl.it/@appbrewery/rock-paper-scissors-debugged-end
+
+# ==================================================================
+# Day 5
+
+# v1
+total_height = sum(student_heights)
+number_of_studentes = len(students_heights)
+average_height = round(total_height / number_of_studentes)
+print(average_height)
+
+# v2
+# 🚨 Don't change the code below 👇
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+  student_heights[n] = int(student_heights[n])
+# 🚨 Don't change the code above 👆
+
+
+#Write your code below this row 👇
+
+# count = 0
+# height = 0
+# 
+# for height in student_heights:
+#     height = height + int(student_heights[count])
+#     count += 1
+# print(height/(len(student_heights)-1))
+
+count = 0
+for height in student_heights:
+    count = count + height
+# print(f"The total height value is {count}")
+
+number_of_students = 0
+for student in student_heights:
+    number_of_students += 1
+# print(f"The total number of students is {number_of_students}")
+
+avg = int(count/number_of_students)
+print(avg)
+
+
+
+
+
+
+# ex3
+
+# 🚨 Don't change the code below 👇
+student_scores = input("Input a list of student scores ").split()
+for n in range(0, len(student_scores)):
+  student_scores[n] = int(student_scores[n])
+print(student_scores)
+# 🚨 Don't change the code above 👆
+
+#Write your code below this row 👇
+
+high = 0
+for score in student_scores:
+    if high < score:
+        high = score
+
+print(f"The highest score in the class is: {high}")
+
+
+
+
+#Write your code below this row 👇
+
+even_sum = 0
+for number in range(0, 101, 2):
+    even_sum += number
+print(even_sum)
+
+
+
+
+#Write your code below this row 👇
+
+n = 0
+for n in range(1, 101):
+    if n % 3 == 0 and n % 5 == 0:
+        print("FizzBuzz")
+    elif n % 3 == 0: 
+        print("Fizz")
+    elif n % 5 == 0:
+        print("Buzz")
+    else:
+        print(f"{n}")
+
+
+
+
+
+# Project
+
+# Go to: https://replit.com/@appbrewery/password-generator-start?v=1
+
+#Password Generator Project
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+#Eazy Level
+# password = ""
+
+# for char in range(1, nr_letters + 1):
+#   password += random.choice(letters)
+
+# for char in range(1, nr_symbols + 1):
+#   password += random.choice(symbols)
+
+# for char in range(1, nr_numbers + 1):
+#   password += random.choice(numbers)
+
+# print(password)
+
+#Hard Level
+password_list = []
+
+for char in range(1, nr_letters + 1):
+  password_list.append(random.choice(letters))
+
+for char in range(1, nr_symbols + 1):
+  password_list += random.choice(symbols)
+
+for char in range(1, nr_numbers + 1):
+  password_list += random.choice(numbers)
+
+print(password_list)
+random.shuffle(password_list)
+print(password_list)
+
+password = ""
+for char in password_list:
+  password += char
+
+print(f"Your password is: {password}")
+
+
+# ==================================================================
+# Day 6
+
+
+
+# ==================================================================
+# Day 7
+
+#Step 1 
+
+word_list = ["aardvark", "baboon", "camel"]
+
+#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+import random
+chosen_word = random.choice(word_list)
+
+#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+guess = input("Guess a letter: ").lower()
+
+#TODO-3 - Check if the letter the user guessed (guess) is one of the leters in the chosen_word.
+for letter in chosen_word:
+    if letter == guess:
+        print("Right")
+    else:
+        print("Wrong")
+
+
+
+
+
+
+
+#Step 2
+
+import random
+word_list = ["aardvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
+
+#Testing code
+print(f'Pssst, the solution is {chosen_word}.')
+
+#TODO-1: - Create an empty List called display.
+#For each letter in the chosen_word, add a "_" to 'display'.
+#So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
+display = []
+word_length = len(chosen_word)
+for _ in range(word_length):
+    display += "_"
+
+guess = input("Guess a letter: ").lower()
+
+#TODO-2: - Loop through each position in the chosen_word;
+#If the letter at that position matches 'guess' then reveal that letter in the display at that position.
+#e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
+for position in range(word_length):
+    letter = chosen_word[position]
+    #print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+    if letter == guess:
+        display[position] = letter
+
+#TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
+#Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
+print(display)
+
+
+
+#Step 3
+
+import random
+word_list = ["aardvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+
+#Testing code
+print(f'Pssst, the solution is {chosen_word}.')
+
+#Create blanks
+display = []
+for _ in range(word_length):
+    display += "_"
+
+#TODO-1: - Use a while loop to let the user guess again. The loop should only stop once the user has guessed all the letters in the chosen_word and 'display' has no more blanks ("_"). Then you can tell the user they've won.
+end_of_game = False
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+
+    #Check guessed letter
+    for position in range(word_length):
+        letter = chosen_word[position]
+        #print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+        if letter == guess:
+            display[position] = letter
+
+    print(display)
+
+    #Check if there are no more "_" left in 'display'. Then all letters have been guessed.
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+
+
 # ==================================================================
 # Day 8
 
-# name = input("What is your name? ")
+def my_function(name):
+  print("Hello {name}")
 
-# def my_function(name):
-#  print(f"Hello {name}")
 
-# my_function(name)
+# ex1 
 
-# ========
+#Write your code below this line 👇
+import math
 
-name = input("What is your name? ")
-location = input("What are you right now? ")
+def paint_calc(height, width, cover):
+    number_of_cans = math.ceil((height * width) / cover)
+    print(f"You'll need {number_of_cans} cans of paint.")
 
-def greet(name, location):
-  print(f"Hi {name}")
-  print(f"We added your location {location} in our databases")
+#Write your code above this line 👆
+# Define a function called paint_calc() so that the code below works.   
 
-greet(location=location, name=name)
+# 🚨 Don't change the code below 👇
+test_h = int(input("Height of wall: "))
+test_w = int(input("Width of wall: "))
+coverage = 5
+paint_calc(height=test_h, width=test_w, cover=coverage)
