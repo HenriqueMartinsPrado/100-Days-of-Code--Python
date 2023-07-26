8e626c6ec9f58a192324ac237598c38eac4e0262
HenriqueMartinsPrado <45240559+HenriqueMartinsPrado@users.noreply.github.com>
2023-07-26 03:20:28 +0000

    day 8 v4

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 # Day 1
 
 # The name of the program to debug is Thonny
@@ -625,17 +626,19 @@ def my_function(name):
 # ex1 
 
 #Write your code below this line 👇
-import math
 
-def paint_calc(height, width, cover):
-    number_of_cans = math.ceil((height * width) / cover)
-    print(f"You'll need {number_of_cans} cans of paint.")
+def prime_checker(number):
+    isPrime = True
+    for n in range(2, number):
+        if (number % n) == 0:
+            isPrime = False
+    if isPrime:
+        print("It's a prime number.")
+    else:
+        print("It's not a prime number.")
 
 #Write your code above this line 👆
-# Define a function called paint_calc() so that the code below works.   
-
-# 🚨 Don't change the code below 👇
-test_h = int(input("Height of wall: "))
-test_w = int(input("Width of wall: "))
-coverage = 5
-paint_calc(height=test_h, width=test_w, cover=coverage)
+    
+#Do NOT change any of the code below👇
+n = int(input("Check this number: "))
+prime_checker(number=n)
