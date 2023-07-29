e387d91bb342a623a80a9b7f4e47bfe9360d3722
HenriqueMartinsPrado <45240559+HenriqueMartinsPrado@users.noreply.github.com>
2023-07-29 00:55:30 +0000

    day 11 v11

--- main.py
+++ main.py
@@ -1181,4 +1181,4 @@ def play_game():
 #Hint 14: Ask the user if they want to restart the game. If they answer yes, clear the console and start a new game of blackjack and show the logo from art.py.
 while input("Do you want to play a game of Blackjack? Type 'y' or 'n': ") == "y":
   clear()
-  play_game()
+  play_game() 
