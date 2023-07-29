9465d19e5255477bb23b46f86bd227f96203bb07
HenriqueMartinsPrado <45240559+HenriqueMartinsPrado@users.noreply.github.com>
2023-07-29 00:20:09 +0000

    day 11 v6

--- main.py
+++ main.py
@@ -59,7 +59,6 @@ cards_dealer = random_cards(cards)
 cards_player = random_cards(cards)
 bet = input("How much are you going to bet? $ ")
 
-
 def sum_cards(cards_list):
     return int(cards_list[0]) + int(cards_list[1])
 
