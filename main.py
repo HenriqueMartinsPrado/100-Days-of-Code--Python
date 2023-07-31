5a81cd89a79e4c133576cffba39e4a77ced848c5
HenriqueMartinsPrado <45240559+HenriqueMartinsPrado@users.noreply.github.com>
2023-07-31 23:48:16 +0000

    day 12 v3

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 
 from art import logo
-
-print("Welcome to the Number Guessing Game!")
-print("I'm thinking of a the number between 1 and 100.")
-
-def computer_number():
-  for n in range(1, 101):
-    print(n)
-
-# level = input("Choose a diffilculty. Type 'easy' or 'hard': ")
+import random
 
 print(logo)
+print("Welcome to the Number Guessing Game!")
+print("I'm thinking of a the number between 1 and 100.")
+level = input("Choose a diffilculty. Type 'easy' or 'hard': ")
+
+answer = random.randint(1, 100)
+
+level_opt = {"easy": 5, "hard": 10}
+is_gameover = False
+
+print(level_opt["easy"])
+
+# def check_level(level):
+#   if level in level_opt:
+#     print("Easy")
+    
+  # elif level_opt[level] == level_opt[1]:
+  #   print("Hard")
+  # else:
+  #   print("You type a invalid level. Choose a level 'easy' or 'hard' ")
