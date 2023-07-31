7b572cadafa4eab8f155a12b94ec2377aaad1da1
HenriqueMartinsPrado <45240559+HenriqueMartinsPrado@users.noreply.github.com>
2023-07-31 21:20:00 +0000

    day 12 v2

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from art import logo
 print("Welcome to the Number Guessing Game!")
 print("I'm thinking of a the number between 1 and 100.")
 
-level = input("Choose a diffilculty. Type 'easy' or 'hard': ")
+def computer_number():
+  for n in range(1, 101):
+    print(n)
+
+# level = input("Choose a diffilculty. Type 'easy' or 'hard': ")
 
 print(logo)
