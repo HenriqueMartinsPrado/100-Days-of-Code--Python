6c5a2905546ddd87ef290e3a3481cdfb096f87f1
HenriqueMartinsPrado <45240559+HenriqueMartinsPrado@users.noreply.github.com>
2023-07-31 23:53:29 +0000

    day 12 v4

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ print(logo)
 print("Welcome to the Number Guessing Game!")
 print("I'm thinking of a the number between 1 and 100.")
 level = input("Choose a diffilculty. Type 'easy' or 'hard': ")
+guess = int(input("Make a guess: "))
 
 answer = random.randint(1, 100)
 
