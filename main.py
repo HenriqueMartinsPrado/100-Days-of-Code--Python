bc7fea29383049b256bd23d1ecf872c7d07f286c
HenriqueMartinsPrado <45240559+HenriqueMartinsPrado@users.noreply.github.com>
2023-08-01 00:33:47 +0000

    day 12 v11

--- main.py
+++ main.py
@@ -13,11 +13,15 @@ def set_diffilculty():
   else:
     return HARD_LEVEL_TURNS
 
-def checK_answer(guess, answer):
+turns = 0
+def checK_answer(guess, answer, turns):
+  """Check answers againt guess. Returns the number od turns remaining"""
   if guess > answer:
     print("Too high")
+    return turns-1
   elif guess < answer:
     print("Too low.")
+    return turns-1
   else:
     print(f"You got it. The answer was {answer}")
 
@@ -28,9 +32,9 @@ def game():
   answer = random.randint(1, 100)
   
   turns = set_diffilculty()
-  print(f"You have {turns} attempts remaining to guess the number.")
   
   guess = 0
   while guess != answer:
+    print(f"You have {turns} attempts remaining to guess the number.")
     guess = int(input("Make a guess: "))
-    checK_answer(guess, answer)
+    turns = checK_answer(guess, answer, turns)
