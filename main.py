b7ae50cbbbf5ba1516b3d89f3e86f15234fa4477
HenriqueMartinsPrado <45240559+HenriqueMartinsPrado@users.noreply.github.com>
2023-08-01 00:03:17 +0000

    day 12 v7

--- main.py
+++ main.py
@@ -5,13 +5,21 @@ import random
 print(logo)
 print("Welcome to the Number Guessing Game!")
 print("I'm thinking of a the number between 1 and 100.")
-level = input("Choose a diffilculty. Type 'easy' or 'hard': ")
 guess = int(input("Make a guess: "))
-
 answer = random.randint(1, 100)
 
-level_opt = {"easy": 5, "hard": 10}
-is_gameover = False
+# level_opt = {"easy": 5, "hard": 10}
+EASY_LEVEL_TURNS = 10
+HARD_LEVEL_TURNS = 5
+
+# is_gameover = False
+
+def set_diffilculty():
+  level = input("Choose a diffilculty. Type 'easy' or 'hard': ")
+  if level == "easy":
+    turns = EASY_LEVEL_TURNS
+  else:
+    turns = HARD_LEVEL_TURNS
 
 
 def checK_answer(guess, answer):
@@ -20,4 +28,4 @@ def checK_answer(guess, answer):
   elif guess < answer:
     print("Too low.")
   else:
-    print("You got it. The answer was {answer}")
+    print(f"You got it. The answer was {answer}")
