7cc763800e04703601924a23d540de2fb0da023d
HenriqueMartinsPrado <45240559+HenriqueMartinsPrado@users.noreply.github.com>
2023-08-04 22:44:01 +0000

    day14 v9

--- main.py
+++ main.py
@@ -7,18 +7,20 @@ import game_data
 import art
 import random
 
-# print(game_data.len())
+# Get the size of my list
+list_size = len(game_data.data)
 
+# First logo in the game
 print(art.logo_higher_lower)
 
 print("Compare A:", game_data.data[0]["name"], ", a", game_data.data[0]["description"], ", from", game_data.data[0]["country"])
 
+# Second logo in the game
 print(art.logo_vs)
 
 print("Against B:", game_data.data[0]["name"], ", a", game_data.data[0]["description"], ", from", game_data.data[0]["country"])
 
 choice = input("Who has more followers? Type A or B \n")
-# print(choice)
 
 
 
