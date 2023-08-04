daacf76584a4a812ed4bd38bdb8e4c77a9f523ea
HenriqueMartinsPrado <45240559+HenriqueMartinsPrado@users.noreply.github.com>
2023-08-04 22:39:13 +0000

    day 14 v8

--- main.py
+++ main.py
@@ -7,22 +7,17 @@ import game_data
 import art
 import random
 
-print(game_data.len())
+# print(game_data.len())
 
 print(art.logo_higher_lower)
 
 print("Compare A:", game_data.data[0]["name"], ", a", game_data.data[0]["description"], ", from", game_data.data[0]["country"])
 
-# print(f"Compare A: {game_data.data[0]["name"]}, a {game_data.data[0]["description"]}, from {game_data.data[0]["country"]}")
-
-
-# print("Compare A: Ronaldinho, a Footballer, from Brazil")
 print(art.logo_vs)
 
 print("Against B:", game_data.data[0]["name"], ", a", game_data.data[0]["description"], ", from", game_data.data[0]["country"])
 
-
-# choice = input(print("Who has more followers? Type A or B"))
+choice = input("Who has more followers? Type A or B \n")
 # print(choice)
 
 
