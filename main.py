b10f9783f3bfc374247c0b2ac3047d5887199574
HenriqueMartinsPrado <45240559+HenriqueMartinsPrado@users.noreply.github.com>
2023-08-04 22:01:55 +0000

    day 14 v6

--- main.py
+++ main.py
@@ -5,13 +5,14 @@
 
 import game_data
 import art
+import random
 
 print(art.logo_higher_lower)
 
-print("Compare A: Ronaldinho, a Footballer, from Brazil")
+print(game_data.data[1]["name"])
 
-print(art.logo_vs)
-
-print("Against B: Khloé Kardashian, a Reality TV personality and businesswoman, from United States.")
-
-choice = input(print("Who has more followers? Type A or B"))
+# print("Compare A: Ronaldinho, a Footballer, from Brazil")
+# print(art.logo_vs)
+# print("Against B: Khloé Kardashian, a Reality TV personality and businesswoman, from United States.")
+# choice = input(print("Who has more followers? Type A or B"))
+# print(choice)
